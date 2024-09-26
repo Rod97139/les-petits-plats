927844c4d84ec27b1c7705f9e2255d5f3a6a8d0f
Rod97139 <fayaflame@gmail.com>
2024-09-26 03:18:35 +0200

    search tag update

--- assets/js/templates/optionsTemplate.js
+++ assets/js/templates/optionsTemplate.js
@@ -13,7 +13,7 @@ export const optionsTemplate = (type, App) => {
     const $labelSearchContainerByType = document.querySelector(`.label-search-${type}`)
     $labelSearchContainerByType.innerHTML = ""
     tagSearchInput.setAttribute("type", "search");
-    tagSearchInput.classList.add('mx-2', 'md:mx-1', 'w-vw-24','md:w-[180px]' , 'p-2', 'rounded', 'border', 'border-gray-300', 'mb-2', 'text-sm', 'focus:outline-none', 'focus:border-gray-600', 'placeholder-gray-500');
+    tagSearchInput.classList.add('ml-1', 'md:mx-1','w-[177px]', 'max-[584px]:w-vw-30','md:w-[180px]' , 'p-2', 'rounded', 'border', 'border-gray-300', 'mb-2', 'text-sm', 'focus:outline-none', 'focus:border-gray-600', 'placeholder-gray-500');
     tagSearchInput.setAttribute("id", `tag-search-${type}`);
     tagSearchInput.addEventListener('keyup', (e) => {
         const input = e.target.value.toLowerCase()
